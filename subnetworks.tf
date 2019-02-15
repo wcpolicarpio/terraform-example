e2f9831d88f8c5238428b3f9dce70b5b9669ff05
Walter Policarpio <walter.policarpio@a4media.com>
2019-02-15 10:07:42 -0500

    AWS Server

---
+++ subnetworks.tf
@@ -0,0 +1,36 @@
+
+resource "aws_vpc" "environment-example-two" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support =  true
+  tags {
+    Name = "terraform-aws-vpc-example-two"
+  }
+
+}
+
+resource "aws_subnet" "subnet1" {
+  cidr_block = "${cidrsubnet(aws_vpc.environment-example-two.cidr_block, 3, 1)}"
+  vpc_id = "${aws_vpc.environment-example-two.id}"
+  availability_zone = "ca-central-1a"
+}
+
+resource "aws_subnet" "subnet2" {
+  cidr_block = "${cidrsubnet(aws_vpc.environment-example-two.cidr_block, 2, 2)}"
+  vpc_id = "${aws_vpc.environment-example-two.id}"
+  availability_zone = "ca-central-1b"
+}
+
+resource "aws_security_group" "subnetsecurity" {
+  vpc_id = "${aws_vpc.environment-example-two.id}"
+
+  ingress {
+    cidr_blocks = [
+      "${aws_vpc.environment-example-two.cidr_block}"
+    ]
+
+    from_port = 80
+    protocol = "tcp"
+    to_port = 80
+  }
+}
